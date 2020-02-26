9e6413b679745b353f58a1cb7e66abc3aa411b86
MartinPankraz <m.pankraz@gmx.de>
2020-02-26 15:20:33 +0000

    beta version: master view + details page, navigation + data synch

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -15,22 +15,79 @@ sap.ui.define([
 				targetPlatformLink = "/azure/manual/paths/invoke";
 			}else {
 				//must be Azure then
-				targetPlatformLink = sRunOnPlatform
+				targetPlatformLink = sRunOnPlatform;
 			}
-			this.oModel = new sap.ui.model.json.JSONModel(targetPlatformLink);
+			
 			/*this.oModel.attachRequestCompleted(function() {
 		        console.log(that.oModel.getData());
 		    });*/
-		    this.getView().setModel(this.oModel,"azure");
+			that.oModel = new sap.ui.model.json.JSONModel();
+    		that.oModelDetail = new sap.ui.model.json.JSONModel();
+    		
+			this.synch();
 			
-			/*var url = "/azure/manual/paths/invoke";
+    		that.getView().setModel(that.oModel,"azure");
+    		that.getView().setModel(that.oModelDetail,"detail");
+		},
+		
+		synch: function(){
+			var url = "/azure/manual/paths/invoke";
+			var that = this;
 			$.ajax({
 				type : 'GET',
 				url: url,
 		        success: function(data){
-		            console.log(data);
+		            var sapTable = data[0];
+		            var sentimentTable = data[1];
+		            for(var i=0;i< sapTable.value.length;i++){
+		            	var item = sapTable.value[i];
+		            	var result = sentimentTable.value.filter(obj => {
+						  return obj.RowKey == item.RowKey;
+						});
+						var averageScore = -1;
+						var sum = 0;
+		            	for(var j=0;j<result.length;j++){
+		            		sum += result[j].score;
+		            	}
+		            	averageScore = sum / result.length;
+		            	//enrich model with sentiment from twitter table, join by RowKey (AirlineCode)
+		            	item.sentiment = averageScore;
+		            }
+		    		that.oModel.setData(sapTable);
+		    		that.oModelDetail.setData(sentimentTable);
 		        }
-			});*/
+			});
+		},
+		
+		myItemPress: function(oEvent){
+			var navCon = this.byId("parentNavContainer");
+			var oContext = oEvent.getParameter("listItem").getBindingContext("azure");
+			var path = oContext.getPath();
+			var airlineCode = oContext.getModel().getProperty(path).RowKey;
+			
+			var oBinding = this.byId("idDetailsTable").getBinding("items");
+			oBinding.filter(new sap.ui.model.Filter("RowKey", "EQ", airlineCode ));
+			
+			navCon.to(this.byId("detail"));
+		},
+		
+		navBackPress:function(oEvent){
+			var navCon = this.byId("parentNavContainer");
+			navCon.back();
+		},
+		
+		statusColor: function(sNumber){
+			var result = "None";
+			if(sNumber < 0.4){
+				result = "Error";
+			}else if(sNumber < 0.6){
+				result = "Warning";
+			}else if(sNumber <= 1.0){
+				result = "Success";
+			}else{
+				result = "None";
+			}
+			return result;
 		}
 	});
 });
